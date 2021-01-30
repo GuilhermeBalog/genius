ef286e0f0d77eb6a64b9df2b2d3b18c61d87d271
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2021-01-29 21:08:43 -0300

    Formatação de código

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Genius é um brinquedo clássico.
 
 Ele toca uma sequência e você tem que repetir apertando os botões. E a sequência vai aumento progressivamente
 
-Você pode jogar [aqui no meu site](https://guilhermebalog.github.io/genius)
+Você pode jogar [aqui no meu site](https://guilhermebalog.ga/genius)
 
 Mas se quiser baixar e rodar na sua máquina, clone o repositório com
 
@@ -14,7 +14,7 @@ git clone clone https://github.com/GuilhermeBalog/genius.git
 
 E acesse o arquivo `index.html` no seu navegador favorito!
 
-> Confira o arquivo main.js para ver a implementação. 
-> Sinta-se livre pra dar um fork no repoditório e adicionar suas ideias
+> Confira o arquivo main.js para ver a implementação.
+> Sinta-se livre pra dar um fork no repositório e adicionar suas ideias
 
 *That's all folks! Keep coding!*
--- main.js
+++ main.js
@@ -1,133 +1,131 @@
 const Genius = {
-    nomeDasCores: ['green', 'red', 'yellow', 'blue'],
-    notas: [261.6, 293.7, 329.6, 392.0],
+  nomeDasCores: ["green", "red", "yellow", "blue"],
+  notas: [261.6, 293.7, 329.6, 392.0],
 
-    sequencia: [],
-    sequenciaDoJogador: [],
-    gameOver: true,
-    contagemDeRodadas: 0,
-    limiteDeRodadas: 6,
+  sequencia: [],
+  sequenciaDoJogador: [],
+  gameOver: true,
+  contagemDeRodadas: 0,
+  limiteDeRodadas: 6,
 
-    titulo: document.querySelector('#root > h1'),
-    botaoDePlay: document.querySelector('#root > button'),
+  titulo: document.querySelector("#root > h1"),
+  botaoDePlay: document.querySelector("#root > button"),
 
-    comecarJogo(){
-        this.contagemDeRodadas = 0
-        this.gameOver = false
-        this.sequencia = []
-        this.botaoDePlay.innerHTML = 'Recomeçar'
-        this.escrever('Genius!')
-        this.habilitarBotoes()
-        this.novaRodada()
-    },
+  comecarJogo() {
+    this.contagemDeRodadas = 0;
+    this.gameOver = false;
+    this.sequencia = [];
+    this.botaoDePlay.innerHTML = "Recomeçar";
+    this.escrever("Genius!");
+    this.habilitarBotoes();
+    this.novaRodada();
+  },
 
-    novaRodada(){
-        this.contagemDeRodadas++
-        this.sequenciaDoJogador = []
-        this.gerarToque()
-        this.tocarSequencia()
-    },
+  novaRodada() {
+    this.contagemDeRodadas++;
+    this.sequenciaDoJogador = [];
+    this.gerarToque();
+    this.tocarSequencia();
+  },
 
-    jogar(indiceDaCor){
-        this.tocarNota(indiceDaCor)
+  jogar(indiceDaCor) {
+    this.tocarNota(indiceDaCor);
 
-        if(this.gameOver) return 
+    if (this.gameOver) return;
 
+    this.sequenciaDoJogador.push(indiceDaCor);
 
-        this.sequenciaDoJogador.push(indiceDaCor)
+    this.sequenciaDoJogador.forEach((jogada, index) => {
+      if (jogada != this.sequencia[index]) {
+        this.finalizarJogo("Você Perdeu!");
+      }
+    });
 
-        this.sequenciaDoJogador.forEach((jogada, index) => {
-            if(jogada != this.sequencia[index]){
-                this.finalizarJogo('Você Perdeu!')
-            }
-        })
+    if (this.gameOver) return;
 
-        if(this.gameOver) return
-
-        if(this.sequenciaDoJogador.length == this.sequencia.length){
-
-            if(this.contagemDeRodadas >= this.limiteDeRodadas){
-                this.finalizarJogo('Você Ganhou!')
-            } else {
-                this.novaRodada()
-            }
-        }
-    },
-    
-    gerarToque(){
-        const randomInt = this.gerarNumeroAleatorio()
-        this.sequencia.push(randomInt)
-    },
-
-    gerarNumeroAleatorio(){
-        return Math.floor(Math.random() * 4)
-    },
-
-    tocarSequencia(){
-        this.desabilitarBotoes()
-
-        let iteradorDaSequencia = 0
-        let devoClarear = true
-
-        const velocidade = 500
-
-        const intervalo = setInterval(() => {
-            const indiceDaSequencia = this.sequencia[iteradorDaSequencia]
-            const cor = this.nomeDasCores[indiceDaSequencia]
-            const botaoDeCor = document.getElementById(cor)
-
-            if(devoClarear){
-                botaoDeCor.style.backgroundColor = 'white'
-                this.tocarNota(indiceDaSequencia)
-            } else {
-                botaoDeCor.style.backgroundColor = cor
-                iteradorDaSequencia++ 
-            }
-
-            if(iteradorDaSequencia >= this.sequencia.length){
-                clearInterval(intervalo)
-                this.habilitarBotoes()
-            }
-
-            devoClarear = !devoClarear
-
-        }, velocidade)
-
-    },
-
-    habilitarBotoes(){
-        const botoes = document.querySelectorAll('#buttons button')
-        botoes.forEach(botao => {
-            botao.removeAttribute('disabled')
-        })
-    },
-
-    desabilitarBotoes(){
-        const botoes = document.querySelectorAll('#buttons button')
-        botoes.forEach(botao => {
-            botao.setAttribute('disabled', 'disabled')
-        })
-    },    
-
-    finalizarJogo(mensagem){
-        this.gameOver = true
-        this.escrever(mensagem)
-        this.desabilitarBotoes()
-    },
-
-    escrever(texto){
-        this.titulo.innerHTML = texto
-    },
-
-    tocarNota(indiceDaNota){
-        const synth = new Tone.Synth().toDestination()
-        synth.triggerAttackRelease(this.notas[indiceDaNota], '8n')
+    if (this.sequenciaDoJogador.length == this.sequencia.length) {
+      if (this.contagemDeRodadas >= this.limiteDeRodadas) {
+        this.finalizarJogo("Você Ganhou!");
+      } else {
+        this.novaRodada();
+      }
     }
-}
+  },
 
-document.querySelector('#titulo + button').onclick = () => { Genius.comecarJogo() }
-document.querySelectorAll('#buttons button').forEach((botao, index) => {
-    botao.onclick = () => {
-        Genius.jogar(index)
-    }
-})
+  gerarToque() {
+    const randomInt = this.gerarNumeroAleatorio();
+    this.sequencia.push(randomInt);
+  },
+
+  gerarNumeroAleatorio() {
+    return Math.floor(Math.random() * 4);
+  },
+
+  tocarSequencia() {
+    this.desabilitarBotoes();
+
+    let iteradorDaSequencia = 0;
+    let devoClarear = true;
+
+    const velocidade = 500;
+
+    const intervalo = setInterval(() => {
+      const indiceDaSequencia = this.sequencia[iteradorDaSequencia];
+      const cor = this.nomeDasCores[indiceDaSequencia];
+      const botaoDeCor = document.getElementById(cor);
+
+      if (devoClarear) {
+        botaoDeCor.style.backgroundColor = "white";
+        this.tocarNota(indiceDaSequencia);
+      } else {
+        botaoDeCor.style.backgroundColor = cor;
+        iteradorDaSequencia++;
+      }
+
+      if (iteradorDaSequencia >= this.sequencia.length) {
+        clearInterval(intervalo);
+        this.habilitarBotoes();
+      }
+
+      devoClarear = !devoClarear;
+    }, velocidade);
+  },
+
+  habilitarBotoes() {
+    const botoes = document.querySelectorAll("#buttons button");
+    botoes.forEach((botao) => {
+      botao.removeAttribute("disabled");
+    });
+  },
+
+  desabilitarBotoes() {
+    const botoes = document.querySelectorAll("#buttons button");
+    botoes.forEach((botao) => {
+      botao.setAttribute("disabled", "disabled");
+    });
+  },
+
+  finalizarJogo(mensagem) {
+    this.gameOver = true;
+    this.escrever(mensagem);
+    this.desabilitarBotoes();
+  },
+
+  escrever(texto) {
+    this.titulo.innerHTML = texto;
+  },
+
+  tocarNota(indiceDaNota) {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease(this.notas[indiceDaNota], "8n");
+  },
+};
+
+document.querySelector("#titulo + button").onclick = () => {
+  Genius.comecarJogo();
+};
+document.querySelectorAll("#buttons button").forEach((botao, index) => {
+  botao.onclick = () => {
+    Genius.jogar(index);
+  };
+});
